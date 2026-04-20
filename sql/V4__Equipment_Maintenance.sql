f014ec4d8112019b1fd3107fcdac1ca54611af32
julio-miranda <julioarmandomirandareyes@gmail.com>
2026-04-20 17:21:37 -0600

    Borrado accidental de carpeta sql

--- sql/V4__Equipment_Maintenance.sql
+++
@@ -1,7 +0,0 @@
--- V4__Equipment_Maintenance.sql
--- Evolución del esquema para control de mantenimiento.
-
-ALTER TABLE equipos
-    ADD COLUMN ultima_revision DATE,
-    ADD COLUMN responsable_mantenimiento VARCHAR(150),
-    ADD COLUMN observaciones_mantenimiento TEXT;
